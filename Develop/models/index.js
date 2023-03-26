fb26b1a9f1eaa00648baa6217178b5cc6f71ff0d
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-03-26 13:19:36 -0500

    associations complete and env corrected

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, 
+  {
+    foriegnKey: 'category_id'
+  });
 
-// Categories have many Products
+Category.hasMany(Product, 
+  {
+  foriegnKey: 'category_id'
+  });
 
-// Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, 
+  {
+    through: 'ProductTag', 
+    foriegnKey: 'product_id'
+  });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Tag, 
+  {
+    through: 'ProductTag',
+    foriegnKey: 'tag_id'
+  });
 
 module.exports = {
   Product,
